8c8daf5d6643c5ce2f82f100205f0826068958f6
Arthur Emanoel <arthurcorreia839@gmail.com>
2026-05-29 14:12:37 -0300

    mudanças para ver o conflito na feature

--- helloword.c
+++ helloword.c
@@ -2,10 +2,10 @@
 
 int main (){
     printf("Hello, world!");
-    printf("Mini curso");
+    int i;
 
     int soma;
-    for (int i=0; i< 10; i++){
+    for (i=0; i< 10; i++){
         soma += i;
     }
 }
